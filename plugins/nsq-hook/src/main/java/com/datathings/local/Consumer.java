e58d71f3b0fb1bfe7f833b652c8e903180a13c2d
lpotages <lpotages@gmail.com>
2017-04-21 10:32:49 +0200

    Adding local consumer (test purpose) to nsq-hook

---
+++ plugins/nsq-hook/src/main/java/com/datathings/local/Consumer.java
@@ -0,0 +1,31 @@
+package com.datathings.local;
+
+import com.github.brainlag.nsq.NSQConsumer;
+import com.github.brainlag.nsq.lookup.DefaultNSQLookup;
+import com.github.brainlag.nsq.lookup.NSQLookup;
+
+import java.io.UnsupportedEncodingException;
+
+public class Consumer
+{
+    public static void main( String[] args )
+    {
+        NSQLookup lookup = new DefaultNSQLookup();
+        lookup.addLookupAddress("localhost", 4161);
+        NSQConsumer consumer = new NSQConsumer(lookup, "Greycat", "MyChannel", (message) -> {
+
+            try {
+                System.out.println("Received msg: " +  new String(message.getMessage(), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            //now mark the message as finished.
+            message.finished();
+
+            //or you could requeue it, which indicates a failure and puts it back on the queue.
+            //message.requeue();
+        });
+
+        consumer.start();
+    }
+}
