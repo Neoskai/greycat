75625519dfe245820c1d46d164390efb37704ac0
lpotages <lpotages@gmail.com>
2017-06-07 17:39:28 +0200

    Adding basic timeline to web

--- plugins/backup/src/main/web/src/App.js
+++ plugins/backup/src/main/web/src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Timeline from 'react-visjs-timeline'
+
 import './App.css';
 
+const options = {
+    width: '100%',
+    height: '150px',
+    showMajorLabels: true,
+    showCurrentTime: true,
+    zoomMin: 100000,
+    selectable:true,
+    type: 'background',
+    format: {
+        minorLabels: {
+            minute: 'h:mma',
+            hour: 'ha'
+        }
+    }
+};
+
+function timeToDate(timestamp){
+    return new Date(timestamp*1000);
+}
+
+var items = [
+    {
+        id: 1,
+        start: timeToDate(1496321093),
+        content: 'Major Snapshot',
+        type: 'box'
+    }];
+
+function onSelect(event){
+    console.log("Selected nodes:");
+    console.log(event.items);
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor({initialGraph}) {
+        super();
+        this.state = {
+            graph: initialGraph
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Backup Gui</h2>
+                </div>
+                <div className="App-content">
+                    <Timeline options={options}
+                              items={items}
+                              selectHandler={onSelect}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ plugins/backup/src/main/web/src/Snapshot.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Snapshot extends Component{
+
+    getSnapshots = function(){
+
+    };
+
+    render(){
+
+    }
+}
